07342de4babeb44125b224c20ac1300f3df87272
MeepMan <ateshreddy@gmail.com>
2025-03-22 11:58:21 +0200

    Commiting email and admin update

--- insert.py
+++ insert.py
@@ -2,7 +2,8 @@ from app import db, app
 from app.models import User
 
 with app.app_context():
-    u = User.query.filter_by(email='ateshreddy@gmail.com').first()
-    u.role = 'admin'
+    u = User(email="mprevani2@gmail.com", campus="Durban", role="student")
+    u.set_password("password")
+    db.session.add(u)
     db.session.commit()
-    print(u.role)
+    print(u)
