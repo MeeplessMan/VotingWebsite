6b629290d3d866cebc0a808831b99c3ce3c0bc18
MeepMan <ateshreddy@gmail.com>
2025-03-25 06:09:51 +0200

    final adjustments to app before deployment

--- insert.py
+++ insert.py
@@ -3,4 +3,7 @@ from app.models import User, Vote, Ballot, Candidate, Election
 
 with app.app_context():
     vote = Vote.query.all()
+    for v in vote:
+        db.session.delete(v)
+    db.session.commit()
     print(vote)
